35a7af16e9b513b177123c5eda8a84d7816d8635
Danchiwaz <mainadanielchiwaz@gmail.com>
2023-08-03 18:35:04 +0300

    added the header to the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environments';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
+
+  headerFortheApp: string = 'writing the angular code is just so fun ';
 }
